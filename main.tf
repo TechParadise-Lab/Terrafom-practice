ed56ecfab406fac2572ceca1646e6503eab166da
Midhun <midhun.kalaivasan@outlook.com>
2026-06-30 21:04:59 +0530

    updated disk size

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    disk_size_gb         = 64
+    disk_size_gb         = 127
   }
 
   source_image_reference {
