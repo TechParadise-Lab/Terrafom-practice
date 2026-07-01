58387cc575898ecdc57fdaa89ba31309a7d822d0
Midhun <midhun.kalaivasan@outlook.com>
2026-07-01 22:51:34 +0530

    updated the backend details

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "tfstatestrg01"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
