80b9bfdbfd920b90126960e7d6fb6daae7da9c05
Midhun <midhun.kalaivasan@outlook.com>
2026-07-01 23:01:22 +0530

    variables

--- variables.tf
+++ variables.tf
@@ -21,15 +21,3 @@ variable "admin_password" {
   type        = string
   sensitive   = true
 }
-
-variable "subscription_id" {
-  description = "Subscription id details"
-  type        = string
-  default     = "d245d804-e720-4143-9cf5-6756b27f60a4"
-}
-
-variable "tenant_id" {
-  description = "tenant id details"
-  type        = string
-  default     = "60adeb65-cbf6-43d5-bb4a-892ad01855be"
-}
